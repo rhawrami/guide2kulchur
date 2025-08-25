1f26a625c37e2f78917bdd1e9d04cb4c9649707e
r h <ravanhawrami@gmail.com>
2025-08-25 11:28:03 -0500

    change type on a db table col

---
+++ db/migrations/001_alter_TABLEalexandria_COLUMNsim_books.sql
@@ -0,0 +1,11 @@
+-- RAN:
+    -- Mon Aug 25 2025
+
+-- change sim_books column in alexandria from jsonb to text[]
+-- reason for change:
+    -- I don't need to have a list of ID/title/author dicts anymore
+    -- I can just have array of ID strings, and merge for other data if needed
+
+ALTER TABLE alexandria
+    ALTER COLUMN sim_books text[];
+
